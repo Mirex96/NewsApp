4d6514ea4c920bc2a16fc66d12f109ef5f56b59e
Dyachenko Stanislav <boar_100@mail.ru>
2022-06-18 13:29:01 +0500

    commit - 2

--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -1,2 +1,8 @@
 package com.example.newsapp.utils
 
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "99200ea26fa94e87abb5bc110ce547a8"
+    }
+}
